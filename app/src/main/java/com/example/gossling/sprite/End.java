489b086bd1e54cc9829a4ee15a39e1f3c06f3bdb
Cirulnichek <tegor247@gmail.com>
2024-01-23 00:22:36 +0300

    end

---
+++ app/src/main/java/com/example/gossling/sprite/End.java
@@ -0,0 +1,54 @@
+package com.example.gossling.sprite;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class End {
+    private Context context;
+    private Bitmap img;
+    private int height, width;
+    float x, y;
+
+    public End(Context context, int id, float x, float y) {
+        this.context = context;
+        img = BitmapFactory.decodeResource(context.getResources(), id);
+
+        this.x = x;
+        this.y = y;
+
+        this.height = img.getHeight();
+        this.width = img.getWidth();
+    }
+
+    public Bitmap getImg() {
+        return img;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void draw(Canvas canvas, Paint paint) {
+        canvas.drawBitmap(img, x, y, paint);
+    }
+
+    public Rect getBoundingBoxRect () {
+        return new Rect((int)x, (int)y, (int)(x + width), (int)(y + height));
+    }
+}
